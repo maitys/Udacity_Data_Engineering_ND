2ebbc4480aaf11cced004ce3c7a6814435d5455d
Siddharth Maity <maitys@outlook.com>
2021-04-04 15:31:03 -0400

    script updates

--- Data_Modeling/2_Relational_Data_Models/Project_1_Data_Modeling_with_Postgres/etl.py
+++ Data_Modeling/2_Relational_Data_Models/Project_1_Data_Modeling_with_Postgres/etl.py
@@ -2,42 +2,81 @@ import os
 import glob
 import psycopg2
 import pandas as pd
+import numpy as np
 from sql_queries import *
 
-
+# ######################
+# process_song_file
+# ######################
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, typ="series")
+    df = pd.DataFrame([df])
+    
+    # fix columns
+    df.columns = ["num_songs", "artist_id", "artist_latitude", "artist_longitude", "artist_location", "artist_name", "song_id", "title", "duration", "year"]
+    df["num_songs"] = df["num_songs"].astype(int)
+    df["artist_id"] = df["artist_id"].astype(str)
+    df["artist_latitude"] = df["artist_latitude"].astype(float)
+    df["artist_longitude"] = df["artist_longitude"].astype(float)
+    df["artist_location"] = df["artist_location"].replace(r'', np.NaN)
+    df["artist_location"] = df["artist_location"].astype(str)
+    df["artist_name"] = df["artist_name"].astype(str)
+    df["song_id"] = df["song_id"].astype(str)
+    df["title"] = df["title"].astype(str)
+    df["duration"] = df["duration"].astype(str)
+    # df["year"] = df["year"].replace(0, np.NaN)
+    df["year"] = df["year"].astype(int)
 
     # insert song record
-    song_data = 
+    song_data = list(df[["song_id", "title", "artist_id", "year", "duration"]].values[0])
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = list(df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]].values[0])
     cur.execute(artist_table_insert, artist_data)
 
-
+# ######################
+# process_log_file
+# ######################
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
+    
+    # fix columns
+    df.columns = ['artist', 'auth', 'firstName', 'gender', 'itemInSession', 'lastName', 'length', 'level', 'location', 'method', 'page','registration', 'sessionId', 'song', 'status', 'ts', 'userAgent', 'userId']
+    df["length"] = df["length"].astype(str)
+    df["registration"] = df["registration"].astype(float)
+    df["sessionId"] = df["sessionId"].astype(int)
+    df["status"] = df["status"].astype(int)
+    # df["ts"] = df["ts"].astype(int)
+    df = df.sort_values(by="ts").reset_index(drop=True)
+    df["userId"] = df["userId"].replace(r"", 0)
+    df["userId"] = df["userId"].astype(int)
+    df["ts"] = pd.to_datetime(df['ts'], unit='ms') # .dt.tz_localize('UTC').dt.tz_convert('America/New_York')
 
     # filter by NextSong action
-    df = 
+    df = df[df["page"]=="NextSong"]
 
-    # convert timestamp column to datetime
-    t = 
-    
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_df = df[["ts"]].copy()
+    time_df.columns = ["start_time"]
+    time_df["hour"] = time_df["start_time"].dt.hour
+    time_df["day"] = time_df["start_time"].dt.day
+    time_df["week"] = time_df["start_time"].dt.isocalendar().week
+    time_df["month"] = time_df["start_time"].dt.month
+    time_df["year"] = time_df["start_time"].dt.year
+    time_df["weekday"] = time_df["start_time"].dt.weekday
+    #t["day_name"] = t["start_time"].dt.day_name()
+    time_df = time_df.sort_values(by="start_time").reset_index(drop=True)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[["userId", "firstName", "lastName", "gender", "level"]].copy()
+    user_df.columns = ["user_id", "first_name", "last_name", "gender", "level"]
+    user_df = user_df.drop_duplicates()
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,7 +95,7 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = songplay_data = [row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent]
         cur.execute(songplay_table_insert, songplay_data)
 
 
@@ -71,6 +110,7 @@ def process_data(cur, conn, filepath, func):
     # get total number of files found
     num_files = len(all_files)
     print('{} files found in {}'.format(num_files, filepath))
+    print(filepath)
 
     # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
--- Data_Modeling/2_Relational_Data_Models/Project_1_Data_Modeling_with_Postgres/sql_queries.py
+++ Data_Modeling/2_Relational_Data_Models/Project_1_Data_Modeling_with_Postgres/sql_queries.py
@@ -2,56 +2,63 @@
 
 songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
 user_table_drop = "DROP TABLE IF EXISTS users;"
-song_table_drop = "DROP TABLE IF EXITS songs;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
 artist_table_drop = "DROP TABLE IF EXISTS artists;"
 time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplays (songplay_id varchar, start_time datetime, user_id varchar, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar);
+CREATE TABLE IF NOT EXISTS songplays 
+(songplay_id serial primary key, start_time timestamp, user_id int, level varchar, song_id varchar, artist_id varchar, session_id int, location varchar, user_agent varchar);
 """)
 
 user_table_create = ("""
-CREATE TABLE IF NOT EXISTS users ();                   
+CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY, first_name varchar, last_name varchar, gender varchar, level varchar);                   
 """)
 
 song_table_create = ("""
-CREATE TABLE IF NOT EXISTS songs ();
+CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, artist_id varchar, year int, duration varchar);
 """)
 
 artist_table_create = ("""
-CREATE TABLE IF NOT EXISTS artists ();
+CREATE TABLE IF NOT EXISTS artists (artist_id varchar, name varchar, location varchar, latitude decimal, longitude decimal);
 """)
 
 time_table_create = ("""
-CREATE TABLE IF NOT EXISTS time ();
+CREATE TABLE IF NOT EXISTS time (start_time timestamp with time zone, hour int, day int, week int, month int, year int, weekday int);
 """)
 
-# INSERT RECORDS
+# # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT(songplay_id) DO NOTHING
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET level = excluded.level
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s)
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s)                       
 """)
 
 
 time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s) 
 """)
 
-# FIND SONGS
+# # FIND SONGS
 
 song_select = ("""
+select a.song_id, b.artist_id from songs a join artists b on (a.artist_id=b.artist_id) where (a.title = %s and b.name = %s and a.duration = %s);
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [song_table_create, artist_table_create, time_table_create, user_table_create, songplay_table_create]
+drop_table_queries = [song_table_drop, artist_table_drop, time_table_drop, user_table_drop, songplay_table_drop]
