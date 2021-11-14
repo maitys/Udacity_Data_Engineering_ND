209b9cdd2da58f4e9d204a49e23bb691bc670e09
Siddharth Maity <maitys@outlook.com>
2021-11-14 17:44:17 -0500

    added docstrings

--- Cloud_Data_Warehouses/2_Implementing_DW_on_AWS/Project_3_Data_Warehouse/create_tables.py
+++ Cloud_Data_Warehouses/2_Implementing_DW_on_AWS/Project_3_Data_Warehouse/create_tables.py
@@ -3,18 +3,35 @@ import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
 def drop_tables(cur, conn):
+    """
+    Summary:
+        This function drops tables in the database should they exist to start fresh on iteration/run as established in the drop_table_queries list in sql_queries.py
+    Args: 
+        conn: for connection to database
+        cur: for cursor object to execute queries
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
-
 def create_tables(cur, conn):
+    """
+    Summary:
+        This function creates the staging tables, fact and dimension tables in the database
+    Args: 
+        conn: for connection to database
+        cur: for cursor object to execute queries
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Summary:
+        This function connects to the database and calls the drop_tables and create_tables functions
+    """
     config = configparser.ConfigParser()
     config.read('maitys_aws_dwh.cfg')
     
--- Cloud_Data_Warehouses/2_Implementing_DW_on_AWS/Project_3_Data_Warehouse/etl.py
+++ Cloud_Data_Warehouses/2_Implementing_DW_on_AWS/Project_3_Data_Warehouse/etl.py
@@ -4,18 +4,36 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Summary:
+        This function loads the staging files from S3 to staging tables in Redshift
+    Args: 
+        conn: for connection to database
+        cur: for cursor object to execute queries
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Summary:
+        This function loads data from staging tables in Redshift to final tables in Redshift
+    Args: 
+        conn: for connection to database
+        cur: for cursor object to execute queries
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Summary:
+        This function connects to the database and calls the load_staging_tables and insert_tables functions
+    """
     config = configparser.ConfigParser()
     config.read('maitys_aws_dwh.cfg')
     
